06aee511fa1bfcdda90487fd001558a92ae267c4
SumanthRH <sumanthrh99@gmail.com>
2024-01-22 18:21:27 -0800

    x

--- ray/ch3.py
+++ ray/ch3.py
@@ -76,15 +76,59 @@ class Policy:
         if explore and random.uniform(0, 1) < epsilon:
             return self.action_space.sample()
         return np.argmax(self.state_action_table[state])
+    
+class Simulation(object): 
+        
+        def __init__(self, env):
+            """Simulates rollouts of an environment, given a policy to follow."""
+            self.env = env
+        def rollout(self, policy, render=False, explore=True, epsilon=0.1): 
+            """Returns experiences for a policy rollout."""
+            experiences = []
+            state = self.env.reset()
+            done = False 
+            while not done:
+                action = policy.get_action(state, explore, epsilon)
+                next_state, reward, done, info = self.env.step(action)
+                experiences.append([state, action, reward, next_state]) 
+                state = next_state
+                if render:
+                    time.sleep(0.05)
+                    self.env.render()
+            return experiences
+        
+def update_policy(policy, experiences, weight=0.1, discount_factor=0.9):
+    """Updates a given policy with a list of (state, action, reward, state) experiences."""
+    for state, action, reward, next_state in experiences:
+        next_max = np.max(policy.state_action_table[next_state])
+        value = policy.state_action_table[state][action]
+        new_value = (1 - weight) * value + weight * \
+                    (reward + discount_factor * next_max)
+        policy.state_action_table[state][action] = new_value
+    return
+
+def train_policy(env, num_episodes=10000, weight=0.1, discount_factor=0.9): 
+    """Training a policy by updating it with rollout experiences."""
+    policy = Policy(env)
+    sim = Simulation(env)
+    for _ in range(num_episodes):
+        experiences = sim.rollout(policy)
+        update_policy(policy, experiences, weight, discount_factor)
+    return policy
 
 if __name__ == "__main__":
     import time
     environment = Environment()
-    random_pos = environment.observation_space.sample()
-    environment.seeker = (random_pos // 5, random_pos % 5)
-    while not environment.is_done():
-        random_action = environment.action_space.sample()
-        environment.step(random_action)
-        time.sleep(0.1)
-        environment.render()
+    untrained_policy = Policy(environment)
+    sim = Simulation(environment)
+    exp = sim.rollout(untrained_policy, render=True, epsilon=1.0)
+    for row in untrained_policy.state_action_table:
+        print(row)
+    # random_pos = environment.observation_space.sample()
+    # environment.seeker = (random_pos // 5, random_pos % 5)
+    # while not environment.is_done():
+    #     random_action = environment.action_space.sample()
+    #     environment.step(random_action)
+    #     time.sleep(0.1)
+    #     environment.render()
         
